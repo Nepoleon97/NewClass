0311b8e1a14ee3bc100ae862827d324966389ad3
Nepoleon97 <nepoleon90@gmail.com>
2022-09-21 19:53:57 +0530

    modify initial code

--- src/test/java/org/fb/GitTask.java
+++ src/test/java/org/fb/GitTask.java
@@ -3,16 +3,16 @@ package org.fb;
 public class GitTask {
 	
 	private void method1() {
-		// TODO Auto-generated method stub
+		
 
 	}
 
 	private void method2() {
-		// TODO Auto-generated method stub
+		
 
 	}
 private void method3() {
-	// TODO Auto-generated method stub
+	
 
 }
 
